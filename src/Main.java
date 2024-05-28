8536354d677f11070a34e243badd0c4a3c52dd2f
Konstantin Vyunkov <constantinent@mail.ru>
2024-05-28 13:23:26 +0300

    initial

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,12 @@ import javax.swing.*;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        int check = 300;  // сумма счета
+        int check = 300; // сумма счета
         int price = 1200; // сумма пополнения
-        int ratio = 100;  // количество рублей за 1 бонус
+        int ratio = 100; // количество рублей за 1 бонус
         int bonus;
 
-        if (price > 1000)  // условие для начисления бонуса
-        {
+        if (price > 1000) { // условие для начисления бонуса
             bonus = price / ratio; // рассчитываем бонус, если условие выполнено
         } else {
             bonus = 0; // рассчитываем бонус, если условие не выполнено
@@ -18,8 +17,8 @@ public class Main {
 
         int total = check + price + bonus; // итоговая сумма вместе с бонусом
 
-        System.out.println("Итоговая сумма на счете: " + total + " ₽"); // по условию задачи мы должны вывести общую сумму (начальная сумма, пополнение и бонус)
-        System.out.println("В том числе бонус за пополнение: " + bonus + " ₽"); // и от дельно сумму бонуса
+        System.out.println("Итоговая сумма на счете: " + total + " ₽"); // по условию задачи мы должны вывести общуюсумму (начальная сумма, пополнение и бонус)
 
+        System.out.println("В том числе бонус за пополнение: " + bonus + " ₽"); // и от дельно сумму бонуса
     }
 }
